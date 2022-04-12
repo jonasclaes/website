6b08628d1e2f17b0994ccfc06ae7b71deabaa3e6
Jonas Claes <jonas@jonasclaes.be>
2022-04-12 11:48:33 +0200

    Added contact form

---
+++ pages/api/contact-form.ts
@@ -0,0 +1,60 @@
+import DOMPurify from "isomorphic-dompurify";
+import { NextApiRequest, NextApiResponse } from "next";
+import validator from 'validator';
+import nodemailer from 'nodemailer';
+import dotenv from 'dotenv';
+
+
+const sendEmail = async (req: NextApiRequest, res: NextApiResponse) => {
+    let { fullName, email, subject, message } = req.body;
+
+    dotenv.config();
+
+    const errorArray = [];
+
+    // Validation
+    if (validator.isEmpty(fullName)) errorArray.push("Full name missing.");
+    if (validator.isEmpty(email)) errorArray.push("E-mail missing.");
+    if (!validator.isEmail(email)) errorArray.push("Invalid e-mail.");
+    if (validator.isEmpty(subject)) errorArray.push("Subject missing.");
+    if (validator.isEmpty(message)) errorArray.push("Message missing.");
+
+    if (errorArray.length > 0) {
+        return res.status(400).json({ error: errorArray });
+    }
+
+    fullName = DOMPurify.sanitize(fullName);
+    subject = DOMPurify.sanitize(subject);
+    message = DOMPurify.sanitize(message);
+
+    const transport = nodemailer.createTransport({
+        host: process.env.MAILER_HOST || "",
+        port: parseInt(process.env.MAILER_PORT || "587"),
+        auth: {
+            user: process.env.MAILER_USER || "",
+            pass: process.env.MAILER_PASS || ""
+        },
+        secure: false
+    });
+
+    const bodyText = `A new contact form submission has arrived!\r\nFull name: ${fullName}\r\nE-mail: ${email}\r\nSubject: ${subject}\r\nMessage: ${message}`
+    const bodyHtml = `A new contact form submission has arrived!<br />Full name: ${fullName}<br />E-mail: ${email}<br />Subject: ${subject}<br />Message: ${message}`
+
+    const mailData: nodemailer.SendMailOptions = {
+        from: 'no-reply@jonasclaes.be',
+        to: 'jonas@jonasclaes.be',
+        subject,
+        text: bodyText,
+        html: bodyHtml
+    }
+
+    try {
+        const messageInfo = await transport.sendMail(mailData);
+    } catch (e) {
+        return res.status(500).json({ error: e })
+    }
+
+    return res.status(200).json({ error: "" })
+}
+
+export default sendEmail;
--- pages/contact.tsx
+++ pages/contact.tsx
@@ -2,9 +2,53 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import Link from 'next/link'
+import { FormEvent, FormEventHandler, useState } from 'react'
 import Layout from '../components/layout'
 
 const Home: NextPage = () => {
+    const [fullName, setFullName] = useState("");
+    const [email, setEmail] = useState("");
+    const [subject, setSubject] = useState("");
+    const [message, setMessage] = useState("");
+    const [sent, setSent] = useState(false);
+
+    const handleSubmit: FormEventHandler = (event: FormEvent) => {
+        event.preventDefault();
+
+        setSent(false);
+
+        postData()
+            .then(data => {
+                console.log(data);
+                setSent(true)
+            })
+            .catch(e => {
+                console.error(e);
+            })
+    }
+
+    const postData = async () => {
+        const res = await fetch("/api/contact-form", {
+            body: JSON.stringify({
+                email,
+                fullName,
+                subject,
+                message
+            }),
+            headers: {
+                "Content-Type": "application/json"
+            },
+            method: "POST"
+        });
+
+        const { error } = await res.json();
+
+        if (error) {
+            console.error(error);
+            throw new Error(error);
+        }
+    }
+
     return (
         <Layout>
             <Head>
@@ -18,35 +62,48 @@ const Home: NextPage = () => {
                 <p className='text-xl md:text-3xl'>I would love to hear from you!</p>
             </main>
 
-            <section className='flex items-center justify-center pb-8'>
+            <section className='grid grid-cols-1 md:grid-cols-2 gap-2 items-start justify-center pb-8'>
                 <div>
-                    <h2 className='text-xl md:text-3xl font-bold'>My contact information:</h2>
+                    <h2 className='text-xl md:text-3xl font-bold'>My contact information</h2>
                     <p><strong>E-mail:</strong> <a href="mailto:jonas@jonasclaes.be" className='text-blue-500'>jonas@jonasclaes.be</a></p>
                     <p><strong>Telephone:</strong> <a href="tel:+32484238902" className='text-blue-500'>+32 484 23 89 02</a></p>
                     <p><strong>Twitter:</strong> <a href="https://twitter.com/jonasclaesbe" className='text-blue-500' target={"_blank"}>@jonasclaesbe</a></p>
                     <p><strong>GitHub:</strong> <a href="https://github.com/jonasclaes" className='text-blue-500' target={"_blank"}>@jonasclaes</a></p>
                 </div>
-                {/* <div className=''>
-                    <h2 className='text-xl md:text-3xl font-bold'>Contact form:</h2>
-                    <form action="/contact" method="post">
+                <div>
+                    <h2 className='text-xl md:text-3xl font-bold'>Contact form</h2>
+                    <form onSubmit={handleSubmit}>
                         <div className='pb-2'>
                             <label htmlFor="name" className='block font-bold'>Full name:</label>
-                            <input type="text" name="name" id="name" placeholder='John Doe' className='bg-gray-900 border-0 w-full rounded-md' />
+                            <input type="text" name="name" id="name" placeholder='John Doe' className='bg-gray-900 border-0 w-full rounded-md' required value={fullName} onChange={(e) => {
+                                setFullName(e.target.value);
+                            }} />
                             <small>Please enter your full name here.</small>
                         </div>
                         <div className='pb-2'>
                             <label htmlFor="email" className='block font-bold'>Your e-mail address:</label>
-                            <input type="email" name="email" id="email" placeholder='john.doe@example.com' className='bg-gray-900 border-0 w-full rounded-md' />
+                            <input type="email" name="email" id="email" placeholder='john.doe@example.com' className='bg-gray-900 border-0 w-full rounded-md' required value={email} onChange={(e) => {
+                                setEmail(e.target.value);
+                            }} />
                             <small>Please enter your e-mail address here.</small>
                         </div>
                         <div className='pb-2'>
-                            <label htmlFor="body" className='block font-bold'>Message text:</label>
-                            <textarea name="body" id="body" placeholder='Your text here...' className='bg-gray-900 border-0 w-full rounded-md' rows={8} />
+                            <label htmlFor="subject" className='block font-bold'>Subject:</label>
+                            <input type="text" name="subject" id="subject" placeholder='An inquiry about ...' className='bg-gray-900 border-0 w-full rounded-md' required value={subject} onChange={(e) => {
+                                setSubject(e.target.value);
+                            }} />
+                            <small>Please enter a subject here.</small>
+                        </div>
+                        <div className='pb-2'>
+                            <label htmlFor="message" className='block font-bold'>Message text:</label>
+                            <textarea name="message" id="message" placeholder='Your text here...' className='bg-gray-900 border-0 w-full rounded-md' rows={8} required value={message} onChange={(e) => {
+                                setMessage(e.target.value);
+                            }} ></textarea>
                             <small>Please enter your message text here.</small>
                         </div>
-                        <button type="submit" className='block bg-blue-600 hover:bg-blue-700 transition-all duration-300 p-2 w-full rounded-md'>Send!</button>
+                        <button type="submit" className={`block bg-blue-600 hover:bg-blue-700 transition-all duration-300 p-2 w-full rounded-md ${ sent ? 'bg-green-600 hover:bg-green-600' : '' }`} disabled={sent}>{ sent ? 'Your message has been sent!' : 'Send!' }</button>
                     </form>
-                </div> */}
+                </div>
             </section>
         </Layout>
     )
