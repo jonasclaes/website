e4e2b9d7eb885d481b2e1b0d8845289487432ce6
Jonas Claes <jonas@jonasclaes.be>
2022-04-18 12:30:02 +0200

    Added Google Analytics scripts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,24 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import Script from "next/script";
 
 function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      {/* Global site tag (gtag.js) - Google Analytics */}
+      <Script src="https://www.googletagmanager.com/gtag/js?id=G-0LMH961K0H" strategy="afterInteractive" />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-0LMH961K0H');
+          `}
+      </Script>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default App;
